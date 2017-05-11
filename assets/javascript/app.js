2de41e0dda8f19371c4b0d1e13c6e29f303b133c
Bartosz Bazański <b.bazanski@visuality.pl>
2017-05-11 15:59:30 +0200

    Add admin panel to change properties of the cats

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,26 +59,35 @@ var controller = {
 var catListView = {
   init: function() {
     this.cats = controller.getAllCats();
-
+    this.list = document.querySelector('.list');
     this.render();
   },
   render: function() {
-    var list = document.querySelector('.list');
+    var list = this.list;
     var itemList;
     this.cats.forEach(function(cat, id) {
       itemList = document.createElement('li');
+      itemList.classList.add('btn');
       itemList.textContent = cat.name;
 
       itemList.addEventListener('click', (function(copyCat) {
         return function() {
           controller.setCurrentCat(copyCat);
           catDisplayView.render();
+          adminPanelView.clear();
         };
       })(cat));
 
       list.append(itemList);
     });
   },
+  updateView: function() {
+    this.clear();
+    this.render();
+  },
+  clear: function() {
+    this.list.innerHTML = '';
+  }
 };
 
 var catDisplayView = {
@@ -108,21 +117,52 @@ var catDisplayView = {
 var adminPanelView = {
   init: function() {
     this.adminButton = document.querySelector('.adminButton');
-    this.adminPanel = document.querySelector('.adminPanel');
+    this.adminPanel = document.querySelector('.adminPanelView');
 
     this.adminButton.addEventListener('click', function() {
       adminPanelView.render();
     });
-  },
-  render: function() {
-    this.cat = controller.getCurrentCat();
+
     this.editCatName = document.createElement('input');
     this.editCatUrl = document.createElement('input');
     this.editCatCounter = document.createElement('input');
+    this.saveButton = document.createElement('button');
+    this.saveButton.classList.add('btn', 'block', 'w-50p');
+    this.saveButton.textContent = "Save";
+    this.clearButton = document.createElement('button');
+    this.clearButton.classList.add('btn', 'block', 'w-50p');
+    this.clearButton.textContent = "Clear";
+    this.clearButton.addEventListener('click', (function(view) {
+      return function() {
+        view.clear();
+      }
+    })(this));
+  },
+  render: function() {
+    this.cat = controller.getCurrentCat();
+
     this.editCatName.value = this.cat.name;
     this.editCatUrl.value = this.cat.imageUrl;
     this.editCatCounter.value = this.cat.clicksCounter;
-    this.adminPanel.append(this.editCatName, this.editCatUrl, this.editCatCounter);
+    this.adminPanel.append(this.editCatName, this.editCatUrl, this.editCatCounter, this.saveButton, this.clearButton);
+    this.saveButton.addEventListener('click', (function(view) {
+      return function() {
+        controller.setCurrentCat(view.updateCat());
+        catDisplayView.render();
+        catListView.updateView();
+        adminPanelView.clear();
+      }
+    })(this));
+  },
+  updateCat: function() {
+    var cat = this.cat;
+    cat.name = this.editCatName.value;
+    cat.imageUrl = this.editCatUrl.value;
+    cat.clicksCounter = this.editCatCounter.value;
+    return cat;
+  },
+  collectCatInfo: function() {
+    var updatedCat
   },
   clear: function() {
     this.adminPanel.innerHTML = '';
